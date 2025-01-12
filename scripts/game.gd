f9d3afe0cf57b29eb69c243e7ff1677888474cbc
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-01-13 02:00:18 +0530

    main menu fix

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,8 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	if GameMode.mode == "host":
+		become_host()
+	elif GameMode.mode == "join":
+		join_as_player()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -10,27 +10,21 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-# Signal function to handle pressing the start button
-func _on_start_button_pressed() -> void:
+
+# Signal function to handle pressing the Host button
+func _on_host_button_pressed() -> void:
 	print("The 'Start Game' button was pressed in the main menu.")
+	GameMode.mode = "host"
 	# Change scene to game scene
 	get_tree().change_scene_to_file("res://scenes/ship_selection.tscn")
 
 
-<<<<<<< Updated upstream
-# Signal function to handle pressing the options button
-func _on_options_button_pressed() -> void:
-	print("The 'Options Game' button was pressed in the main menu.")
-	pass # Replace with function body.
-
-=======
 # Signal function to handle pressing the Join button
 func _on_join_button_pressed() -> void:
 	print("The 'Join Game' button was pressed in the main menu.")
 	GameMode.mode = "join"
 	# Change scene to game scene
 	get_tree().change_scene_to_file("res://scenes/ship_selection.tscn")
->>>>>>> Stashed changes
 
 # Signal function to handle pressing the exit button
 func _on_exit_button_pressed() -> void:
